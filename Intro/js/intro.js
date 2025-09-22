95f8464d5f33329f580ea69b87bd43f268411cff
Aníbal <anibal@myhotel.cl>
2025-09-22 12:48:46 -0300

    Arrays

--- Intro/js/intro.js
+++ Intro/js/intro.js
@@ -25,3 +25,29 @@ console.log(operation(fSum,1,4))
 let fA =  () => console.log("from anonymous fn")
 fA()
 console.log(operation((a, b) => a - b, 5, 5))
+
+// Arrays
+
+// Foreach
+const names = ['Anibal', 'Nicolas', 'Luis']
+names.forEach(name => console.log(name))
+
+// Immutable method
+names.forEach(name => console.log(name.toUpperCase()))
+console.log(names)
+
+// Mutable methods
+names.push('Tino');
+console.log(names)
+names.sort()
+console.log(names)
+
+// Map
+const namesUpper = names.map(name => name.toUpperCase())
+console.log(namesUpper)
+console.log(names)
+
+// Reduce
+const numbers = [5,4,7,1,10]
+const total = numbers.reduce((acc, number) => acc + number, 0)
+console.log(total)
