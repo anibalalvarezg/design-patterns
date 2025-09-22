7e47ef6192e74952abba6624a7cca236d5d5eef9
Aníbal <anibal@myhotel.cl>
2025-09-22 13:06:12 -0300

    OOP

--- Intro/ts/intro.ts
+++ Intro/ts/intro.ts
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+// OOP
+class Drink {
+    private name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    info(): string {
+        return this.name;
+    }
+}
+
+const drink = new Drink("Cristal");
+console.log(drink.info());
