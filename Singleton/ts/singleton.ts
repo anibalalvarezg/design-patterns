90b790f49228014244ba14d553e1ca86978f1d87
Aníbal <anibal@myhotel.cl>
2025-09-23 12:01:28 -0300

    TS - Singleton

---
+++ Singleton/ts/singleton.ts
@@ -0,0 +1,23 @@
+console.log("Singleton");
+
+class Singleton {
+    private static instance: Singleton;
+    public random: number;
+
+    private constructor () {
+        this.random = Math.random();
+    }
+
+    public static getInstance(): Singleton {
+        if (!Singleton.instance) {
+            this.instance = new Singleton();
+        }
+        return this.instance;
+    }
+}
+
+const singleton = Singleton.getInstance();
+const singleton2 = Singleton.getInstance();
+console.log(singleton.random);
+console.log(singleton2.random);
+console.log(singleton === singleton2);
